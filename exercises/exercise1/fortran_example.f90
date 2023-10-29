31a582fd251b78afd12cc318016c87f83f8759c6
Helen Kershaw <hkershaw@ucar.edu>
2023-10-29 14:45:51 -0400

    calculate pi example

--- exercises/exercise1/fortran_example.f90
+++ exercises/exercise1/fortran_example.f90
@@ -1,5 +1,23 @@
-program hello_world
+program calc_pi
 
-print*, 'hello world'
+use ISO_FORTRAN_ENV
 
-end program hello_world
+implicit none
+
+double precision     :: step, x, s, pi
+integer(kind=int64)  :: num_steps, i
+
+num_steps= 500000
+s = 0d0
+step = 1.0d0 / num_steps
+
+do i = 1, num_steps
+   x = (i - 0.5d0) * step
+   s = s + 4.0d0 / (1.0d0 + x*x)
+end do
+
+pi = s * step
+
+print*, 'Calculated value of pi = ', pi
+
+end program calc_pi
